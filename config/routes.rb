cbb933a7b054573cdd2a4f8cb8aafbbdd48745e6
Paulo César <paulo0_0cesar@hotmail.com>
2023-06-11 23:31:38 -0300

    adjusting routes to enable audit path

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@
 
 Rails.application.routes.draw do
   resources :elections do
+    get :audit, on: :member
     resources :questions, shallow: true do
-      resources :answers, shallow: true
+      get :audit, on: :member
+      resources :answers, shallow: true do
+        get :audit, on: :member
+      end
     end
     resources :voters, shallow: true do
+      get :audit, on: :member
       get :ballot, on: :member
       post :submit, on: :member
     end
