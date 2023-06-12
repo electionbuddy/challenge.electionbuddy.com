72c1e0f4dd6eead99c7aa510f9fd1a39dda6c22a
Paulo César <paulo0_0cesar@hotmail.com>
2023-06-11 23:38:19 -0300

    adding auditable concer to election

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Election < ApplicationRecord
+  include Auditable
+
   has_many :questions
+  has_many :voters
   belongs_to :user
 
   serialize :settings, Hash
