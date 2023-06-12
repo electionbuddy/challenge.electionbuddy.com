5a736361402cad50c59cd6ba675f970efbadaa9f
Paulo César <paulo0_0cesar@hotmail.com>
2023-06-12 10:08:17 -0300

    routes adjusts

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       get :ballot, on: :member
       post :submit, on: :member
     end
+    
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
